48d13fa9d3614c1a5f58474179ca4a5d8ae55df9
Verônica Mendes <vellmendes15@gmail.com>
2024-10-23 10:55:59 -0300

    Boost & Bumps

--- Delivery Driver/Assets/Scripts/Delivery.cs
+++ Delivery Driver/Assets/Scripts/Delivery.cs
@@ -5,7 +5,19 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    [SerializeField] Color32 hasPackageColor = new Color32(1, 1, 1, 1);
+    [SerializeField] Color32 noPakageColor = new Color32(1, 1, 1, 1);
+
+    [SerializeField] float destroyDelay = 0.2f;
     bool hasPackage;
+
+    SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
     private void OnCollisionEnter2D(Collision2D other)
     {
         Debug.Log("Ouch!!!");
@@ -13,15 +25,18 @@ public class Delivery : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == "Package")
+        if (other.tag == "Package" && !hasPackage)
         {
             Debug.Log("Package picked up");
             hasPackage = true;
+            spriteRenderer.color = hasPackageColor;
+            Destroy(other.gameObject, destroyDelay);
         }
         else if (other.tag == "Costumer" && hasPackage)
         {
             Debug.Log("Package Delivered");
             hasPackage = false;
+            spriteRenderer.color = noPakageColor;
         }
     }
 }
--- Delivery Driver/Assets/Scripts/Driver.cs
+++ Delivery Driver/Assets/Scripts/Driver.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-    [SerializeField] float steerSpeed = 1f;
-    [SerializeField] float moveSpeed = 0.01f;
+    [SerializeField] float steerSpeed = 250f;
+    [SerializeField] float moveSpeed = 15f;
+    [SerializeField] float slowSped = 10f;
+    [SerializeField] float boostSpeed = 22f;
     void Start() 
     {
       
@@ -16,5 +19,18 @@ public class Driver : MonoBehaviour
         float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         transform.Rotate(0, 0, -steerAmount);
         transform.Translate(0, moveAmount, 0);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        moveSpeed = slowSped;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Boost")
+        {
+            moveSpeed = boostSpeed;
+        }
     }
 }
